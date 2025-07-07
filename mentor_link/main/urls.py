ecd5c3fe372bc55761dedf20c06f20007e77bcd5
marceaugb <marceau.gillio@telecom-sudparis.eu>
2025-07-07 16:54:38 +0200

    Add files via upload
    messagerie fonctionnelle

--- mentor_link/main/urls.py
+++ mentor_link/main/urls.py
@@ -1,18 +1,5 @@
 """
 URL configuration for main project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
@@ -20,29 +7,36 @@ from app_mentorlink import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from app_mentorlink.views import custom_404, custom_500, custom_403, custom_400
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
     path('profil/', views.profil, name='profil'),
-    path('depose_annonce/', views.depose_annonce, name='depose_annonce'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.loginperso, name='login'),
     path('admin/', admin.site.urls),
     path('depose_annonce/', views.depose_annonce, name='depose_annonce'),
     path('confirmation/', views.confirmation, name='confirmation'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('mes-annonces/', views.annonces_utilisateur, name='mes_annonces'),
+    path('mes_annonces/', views.annonces_utilisateur, name='mes_annonces'),
     path('utilisateur/<int:user_id>/annonces/', views.annonces_utilisateur, name='annonces_utilisateur'),
     path('annonce/<int:annonce_id>/', views.annonce_detail, name='annonce_detail'),
     path('annonce/<int:annonce_id>/modifier/', views.modifier_annonce, name='modifier_annonce'),
     path('annonce/<int:annonce_id>/supprimer/', views.supprimer_annonce, name='supprimer_annonce'),
-    path('<slug:slug>/', views.room, name='room'),
     path('annonce/<int:annonce_id>/start-chat/', views.start_private_chat, name='start_private_chat'),
-    path('mes-messages/', views.my_messages, name='my_messages'),
+    path('api/unread-count/', views.get_unread_count, name='unread_count'),
+    # Ajoutez SEULEMENT cette ligne pour les messages :
+    path('messages/', views.all_messages, name='all_messages'),
+    
+    # Gardez votre room original à la fin :
+    path('<slug:slug>/', views.room, name='room'),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = custom_404
+handler500 = custom_500
+handler403 = custom_403
+handler400 = custom_400
